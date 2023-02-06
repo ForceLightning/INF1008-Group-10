747b9666839e1eba202d9c283462166f2fa9426c
houweiced <houwei.c1314@gmail.com>
2023-02-06 16:33:07 +0800

    Update Assignment_Part1_Q3.c

--- Question 3/Assignment_Part1_Q3.c
+++ Question 3/Assignment_Part1_Q3.c
@@ -5,8 +5,8 @@ int start = 50;
 int end = 51;
 
 int insertItem(int input, int position) {   // position: 0 for inserting at the front; 1 for inserting at the end
-    if (start == 0 || end == 100) {
-        printf("Overflow!");
+    if (start == 0 || end == 99) {
+        printf("Overflow!\n");
         return 0;
     }
     if (position == 0) {
@@ -18,13 +18,13 @@ int insertItem(int input, int position) {   // position: 0 for inserting at the 
         end++;
     }
     else {
-        printf("Invalid Input!");
+        printf("Invalid Input!\n");
     }
 }
 
 int removeItem(int position) {              // position: 0 for removing at the front; 1 for removing at the end
     if (end - start == 1) {
-        printf("List is empty!");
+        printf("List is empty!\n");
         return 0;
     }
     if (position == 0) {
@@ -34,44 +34,113 @@ int removeItem(int position) {              // position: 0 for removing at the f
         end--;
     }
     else {
-        printf("Invalid Input!");
+        printf("Invalid Input!\n");
     }
 }
 
 int get(int i) {                            // get(i) returns the item in the list at index i
-    return arr[start + 1 + i];
+    if (i >= 0 && i < end - start - 1) {
+        return arr[start + 1 + i];
+        
+    }
+    else {
+        printf("Index Out Of Range!\n");
+        return NULL;
+    }
+}
+
+int printList() {
+    printf("[")
+    for (int i = 0; i < end - start - 1; i++) {
+        printf("%d,", arr[start + 1 + i]);
+    }
+    printf("]\n");
 }
 
 int main() {
+    // initial list
+    printf("Initial list: ")
+    printList();
+
+    // inserting 10 items to the start of the list
     for (int i = 0; i < 10; i++) {
         insertItem(i + 10, 0);
+    }
+    printf("After adding 10 items to the start of the list: ")
+    printList();
+
+    // inserting 10 items to the end of the list
+    for (int i = 0; i < 10; i++) {
         insertItem(i * 2, 1);
     }
-    for (int i = 0; i < end - start - 1; i++) {
-        printf("%d,", arr[start + 1 + i]);
-    }
-    printf("\n");
+    printf("After adding 10 items to the end of the list: ")
+    printList();
 
-    for (int i = 0; i < 3; i++) {
-        removeItem(0);
+    // inserting the item '13' at the start of the list
+    insertItem(13, 0);
+    printf("After adding the item '13' to the start of the list: ")
+    printList();
+
+    // inserting the item '14' at the end of the list
+    insertItem(14, 1);
+    printf("After adding the item '14' to the start of the list: ")
+    printList();
+
+    // insertimg the item '15' at an invalid position
+    printf("Adding the item '15' to an invalid position:\n")
+    insertItem(15, 2);
+    printf("Current list: ")
+    printList();
+
+    // removing the item at the start of the list
+    removeItem(0);
+    printf("After removing item at the start of the list: ")
+    printList();
+
+    // removing the item at the end of the list
+    removeItem(1);
+    printf("After removing item at the end of the list: ")
+    printList();
+
+    // removing the item at an invalid position
+    printf("Removing item at an invalid position:\n")
+    removeItem(2);
+    printf("Current list: ")
+    printList();
+
+    // get the item at index 5 of the list
+    printf("Item at index 5: %d\n", get(5));
+
+    // get the item at index 20 of the list
+    printf("Getting an item with an index that is out of range:\n");
+    printf("Item at index 20: %d\n", get(20));
+
+    // attempting to overflow at the start of the list
+    printf("Attempting to overflow at the start of the list:\n")
+    for (int i = 0; i < 42; i++) {
+        insertItem(1, 0);
+    }
+    printf("Current list: ")
+    printList();
+
+    // attempting to overflow at the end of the list
+    printf("Attempting to overflow at the end of the list:\n")
+    for (int i = 0; i < 42; i++) {
+        insertItem(0, 1);
+    }
+    printf("Current list: ")
+    printList();
+
+    // removing all the items in the list
+    for (int i = 0; i < end - start - 1; i++) {
         removeItem(1);
     }
-    for (int i = 0; i < end - start - 1; i++) {
-        printf("%d,", arr[start + 1 + i]);
-    }
-    printf("\n");
+    printf("After removing every items in the list: ")
+    printList();
 
-    printf("%d", get(5));
-    printf("\n");
-
-    for (int i = 0; i < 50; i++) {
-        insertItem(40, 1);
-    }
-    for (int i = 0; i < 60; i++) {
-        removeItem(1);
-    }
-    for (int i = 0; i < end - start - 1; i++) {
-        printf("%d,", arr[start + 1 + i]);
-    }
-    printf("\n");
+    // attempting to remove an item from an empty list
+    printf("removing an item from an empty list:\n")
+    removeItem(1);
+    printf("Current list: ")
+    printList();
 }
