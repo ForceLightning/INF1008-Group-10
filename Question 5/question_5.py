8f3e1702630ed329b91b6b50f00e2aeed77ed0fc
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-02-07 20:04:21 +0800

    added unstable implementations to qn 5, updated assignment pdf with listings

--- Question 5/question_5.py
+++ Question 5/question_5.py
@@ -1,13 +1,17 @@
 #Function for selection sort (Stable)
-def selectionSort(array, size):
-
+def selectionSort(array, size, stable=True):
     for ind in range(size):
         min_index = ind
 
-        for j in range(ind + 1, size):
-            # select the minimum element in every iteration
-            if array[j][1] < array[min_index][1]:
-                min_index = j
+        if stable:
+            for j in range(ind + 1, size):
+                # select the minimum element in every iteration
+                if array[j][1] < array[min_index][1]:
+                    min_index = j
+        else:
+            for j in range(size - 1, ind, -1):
+                if array[j][1] < array[min_index][1]:
+                    min_index = j
         # swapping the elements to sort the array
         (array[ind], array[min_index]) = (array[min_index], array[ind])
         print(array)
@@ -15,8 +19,7 @@ def selectionSort(array, size):
 
 
 # Function to do insertion sort (Stable)
-def insertionSort(arr):
-    sorted_arr = []
+def insertionSort(arr, stable=True):
     # Traverse through 1 to len(arr)
     for i in range(1, len(arr)):
 
@@ -25,10 +28,16 @@ def insertionSort(arr):
         # greater than key, to one position ahead
         # of their current position
         j = i-1
-        while j >= 0 and key[1] < arr[j][1]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
+        if stable:
+            while j >= 0 and key[1] < arr[j][1]:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+        else:
+            while j >= 0 and key[1] <= arr[j][1]:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
         print(arr)
     return arr
 
@@ -37,10 +46,13 @@ def insertionSort(arr):
 
 # Python program for implementation of Quicksort Sort (Stable)
 # Function to find the partition position
-def partition(array, low, high):
+def partition(array, low, high, stable=True):
 
     # choose the rightmost element as pivot
-    pivot = array[high][1]
+    if stable:
+        pivot = array[high][1]
+    else:
+        pivot = array[low][1]
 
     # pointer for greater element
     i = low - 1
@@ -48,12 +60,10 @@ def partition(array, low, high):
     # traverse through all elements
     # compare each element with pivot
     for j in range(low, high):
-        if array[j][1] <= pivot:
-
+        if (stable and array[j][1] <= pivot) or (not stable and array[j][1] < pivot):
             # If element smaller than pivot is found
             # swap it with the greater element pointed by i
-            i = i + 1
-
+            i += 1
             # Swapping element at i with element at j
             (array[i], array[j]) = (array[j], array[i])
             print(array)
@@ -64,15 +74,13 @@ def partition(array, low, high):
 
 
 # function to perform quicksort
-
-
-def quickSort(array, low, high):
+def quickSort(array, low, high, stable=True):
     if low < high:
 
         # Find pivot element such that
         # element smaller than pivot are on the left
         # element greater than pivot are on the right
-        pi = partition(array, low, high)
+        pi = partition(array, low, high, stable)
 
         # Recursive call on the left of pivot
         quickSort(array, low, pi - 1)
@@ -81,17 +89,37 @@ def quickSort(array, low, high):
         quickSort(array, pi + 1, high)
 
 
-data_selection = [("d", 4), ("c", 5), ("b", 3), ("a", 3)]
+data_selection = [("", 4), ("", 5), ("first", 3), ("second", 3)]
 #print("Unsorted Array: ", data_selection)
-print("=====Selection Sort=====")
+print("=====Stable Selection Sort=====")
+print(data_selection)
 size = len(data_selection)
 selectionSort(data_selection, size)
 
-print("=====Insertion Sort=====")
-data_insertion = [("d", 4), ("c", 5), ("b", 3), ("a", 3)]
+data_selection = [("", 4), ("", 5), ("first", 3), ("second", 3)]
+print("=====Unstable Selection Sort=====")
+size = len(data_selection)
+print(data_selection)
+selectionSort(data_selection, size, False)
+
+print("=====Stable Insertion Sort=====")
+data_insertion = [("", 4), ("", 5), ("first", 3), ("second", 3)]
+print(data_insertion)
 print(insertionSort(data_insertion))
 
-print("=====Quick Sort=====")
-data_quick = [("d", 4), ("c", 5), ("b", 3), ("a", 3)]
+print("=====Unstable Insertion Sort=====")
+data_insertion = [("", 4), ("", 5), ("first", 3), ("second", 3)]
+print(data_insertion)
+print(insertionSort(data_insertion, False))
+
+print("=====Stable Quick Sort=====")
+data_quick = [("first", 3), ("", 5), ("", 4), ("second", 3)]
+print(data_quick)
 quickSort(data_quick, 0, len(data_quick) - 1)
+print(data_quick)
+
+print("=====Unstable Quick Sort=====")
+data_quick = [("first", 3), ("", 5), ("", 4), ("second", 3)]
+print(data_quick)
+quickSort(data_quick, 0, len(data_quick) - 1, False)
 print(data_quick)
